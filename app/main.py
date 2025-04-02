055ad48ff1b6efc4c1fbbc0db480bf4455817d84
Ivan <plagues.wtp@gmail.com>
2025-04-02 13:41:53 +0300

    initial

---
+++ app/main.py
@@ -0,0 +1,24 @@
+import uvicorn
+from fastapi import FastAPI
+
+from .routes import devices, scans
+from .rabbitmq import rabbit_init
+
+app = FastAPI(title="Scan API")
+
+#* Setup endpoints
+app.include_router(devices.router, prefix="/api/v1/devices", tags=["Devices"])
+app.include_router(scans.router, prefix="/api/v1/scans", tags=["ScanSession"])
+
+@app.get("/health")
+async def health():
+    return {"status": "ok"}
+
+#* Initialize Rammit connection
+@app.on_event("startup")
+async def startup_event():
+    rabbit_init()
+    
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host = "0.0.0.0", port = 8000, reload = True)
+    
